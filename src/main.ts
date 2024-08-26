dfabfaaa5f15cd0e416fd44053e421f7363a71f1
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-08-26 03:07:33 -0600

    refactor: Update CORS origin to allow requests from localhost and ghost-cloud-frontend.vercel.app

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,10 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: ['http://localhost:4321', 'https://ghost-cloud.vercel.app'],
+    origin: [
+      'http://localhost:4321',
+      'https://ghost-cloud-frontend.vercel.app',
+    ],
     credentials: true,
   });
   app.use(
