c4f4e151119210c7fc996908705e0ccb7abb1542
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-08-26 03:14:34 -0600

    refactor: Update app.listen to bind to all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -51,6 +51,6 @@ async function bootstrap() {
     }),
   );
   app.set('trust proxy', 1);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
