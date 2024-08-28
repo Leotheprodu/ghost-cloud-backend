ef3aac2df22c7949c12f2557911b6649e32153f2
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-08-27 18:40:07 -0600

    refactor: Update .env.example and constants.ts to include FRONTEND_URL

--- config/constants.ts
+++ config/constants.ts
@@ -1,6 +1,6 @@
 export const environment = process.env.NODE_ENV;
 export const AppDefaultEmail = 'GhostCloud <auth.ghostcloud.app>';
-export const frontEndUrl = 'http://localhost:4321';
+export const frontEndUrl = process.env.FRONTEND_URL;
 export const groupCampainEmailService = 'ghost-cloud';
 export const tokenCampainEmailService = process.env.TOKEN_MAIL_CAMPAIN;
 
