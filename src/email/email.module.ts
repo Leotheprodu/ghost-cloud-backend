854b72d7ffab015a4b2f554e64e5110fd5a6d807
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-08-28 18:40:59 -0600

    refactor: Update .env.example to include EMAIL_PORT

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -11,6 +11,8 @@ import { AppDefaultEmail } from 'config/constants';
       useFactory: () => ({
         transport: {
           host: process.env.EMAIL_HOST,
+          port: parseInt(process.env.EMAIL_PORT) || 587,
+          secure: false,
           auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD,
